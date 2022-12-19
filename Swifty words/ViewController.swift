1b0086b05040535afd1c5592e7744fb606f32316
markgolubev <markflyingbear@gmail.com>
2022-12-19 11:50:42 +0000

    Initial Commit

---
+++ Swifty words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifty words
+//
+//  Created by Марк Голубев on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
